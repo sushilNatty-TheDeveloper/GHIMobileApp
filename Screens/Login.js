cdd308d96fef3f6b590889298b0833d585216f3e
Sushil Natty <skumar@groceryhaulers.com>
2022-01-04 10:44:37 -0500

    Before changing Navigator

--- Screens/Login.js
+++ Screens/Login.js
@@ -1,68 +1,82 @@
-import React from 'react'
-import { View,TextInput,Button,StyleSheet,  ImageBackground, Dimensions, Keyboard, TouchableNativeFeedback} from 'react-native'
-import MainButton from '../Components/MainButton';
+import React from "react";
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+  Keyboard,
+  TouchableNativeFeedback,
+} from "react-native";
+import MainButton from "../Components/MainButton";
+import Colors from "../constants/colors";
 
-const LoginScreen=props=>{
-
-    return(
-        <TouchableNativeFeedback onPress={()=>Keyboard.dismiss()}>
-        <ImageBackground source={require('../assets/Images/GHIBanner.jpg')} resizeMode='cover' style={Style.Image}  >
-        
-         <View style={Style.mainView}>
-         
+const LoginScreen = (props) => {
+  return (
+    <TouchableNativeFeedback onPress={() => Keyboard.dismiss()}>
+      <ImageBackground
+        source={require("../assets/Images/GHIBanner.jpg")}
+        resizeMode="cover"
+        style={Style.Image}
+      >
+        <View style={Style.mainView}>
           <View style={Style.textInputContiner}>
-            <TextInput style={Style.input} placeholder='User Name'></TextInput>
-            <TextInput style={Style.input} secureTextEntry={true} placeholder='Password'></TextInput>  
-            <MainButton style={Style.button}  >{"Login"}</MainButton>        
-          </View>  
-        
-            
-            
-           
-         </View>
-         </ImageBackground>
-         </TouchableNativeFeedback>
-         
-
-    );
-}
-
-const Style= StyleSheet.create({
-     mainView:{
-         flex:1,
-        // margin:0,
-         
-     },
-      Image:{
-
-        flex: 1,
-        justifyContent: 'center',
-       
-      },
-      textInputContiner:{
-        // flexDirection:'column',
-         justifyContent:'center',
-         alignItems:'center',
-          padding:50, 
-          marginTop:100,         
-         
-       },
-         input:{
-             
-             width:'100%',
-             height:35,
-             borderWidth:1,
-             borderRadius:5,
-             backgroundColor:'white',
-             padding: 6,
-             margin:10,
-             textAlignVertical:'center'
-         },
-         button:{
-
-            width:Dimensions.get('window').width/1.8,
-         }
-        
+            <TextInput style={Style.input} placeholder="User Name"></TextInput>
+            <TextInput
+              style={Style.input}
+              secureTextEntry={true}
+              placeholder="Password"
+            ></TextInput>
+            <MainButton
+              style={Style.button}
+              onPress={() => {
+                props.navigation.navigate({ routeName: "TripView" });
+              }}
+            >
+              {"Login"}
+            </MainButton>
+          </View>
+        </View>
+      </ImageBackground>
+    </TouchableNativeFeedback>
+  );
+};
+LoginScreen.navigationOptions = {
+  headerTitle: "Login",
+  headerStyle: {
+    backgroundColor: Colors.GHIheader,
+  },
+};
+const Style = StyleSheet.create({
+  mainView: {
+    flex: 1,
+    // margin:0,
+  },
+  Image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  textInputContiner: {
+    // flexDirection:'column',
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 50,
+    marginTop: 100,
+  },
+  input: {
+    width: "100%",
+    height: 35,
+    borderWidth: 1,
+    borderRadius: 5,
+    backgroundColor: "white",
+    padding: 6,
+    margin: 10,
+    textAlignVertical: "center",
+  },
+  button: {
+    width: Dimensions.get("window").width / 1.8,
+  },
 });
 
 export default LoginScreen;
